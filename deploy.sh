556a6f63a60f1ecf15466a60a0ed52aed5ba5fb8
leocavalcante <lc@leocavalcante.com>
2017-08-01 17:54:59 -0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ssh multisolution2@multisolution.art.br <<<SSH
+ssh multisolution2@multisolution.art.br <<SSH
   cd public_html/link/
   mv data.json __data.json
 SSH
 
-scp -r build/ multisolution2@multisolution.art.br:public_html/link/
+scp -r build/** multisolution2@multisolution.art.br:public_html/link/
 
-ssh multisolution2@multisolution.art.br <<<SSH
+ssh multisolution2@multisolution.art.br <<SSH
   cd public_html/link/
   mv __data.json data.json
 SSH
